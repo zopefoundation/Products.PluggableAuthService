2bf22f99c4453a92f227871d7542ebba917f2632
Tres Seaver <tseaver@palladion.com>
2007-09-26 02:36:45 +0000

    Tag 1.5.1 release.

---
+++ README.txt
@@ -0,0 +1,1 @@
+(See Products/PluggableAuthService/README.txt).
---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+package = os.path.join(here, 'Products', 'PluggableAuthService')
+
+def _package_doc(name):
+    f = open(os.path.join(package, name))
+    return f.read()
+
+NAME = 'PluggableAuthService'
+
+VERSION = _package_doc('version.txt').strip()
+if VERSION.startswith(NAME):
+    VERSION = VERSION[len(NAME):]
+while VERSION and VERSION[0] in '-_.':
+    VERSION = VERSION[1:]
+
+_boundary = '\n' + ('-' * 60) + '\n'
+README = (open(os.path.join(here, 'README.txt')).read()
+        + _boundary + _package_doc('README.txt')
+        + _boundary + _package_doc('doc/CHANGES.txt')
+         )
+
+setup(name='Products.PluggableAuthService',
+      version=VERSION,
+      description='Pluggable Zope2 authentication / authorization framework',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: System :: Security",
+        "Topic :: System :: Systems Administration :: "
+                "Authentication/Directory",
+        ],
+      keywords='web application server zope zope2',
+      author="Zope Corporation and contributors",
+      author_email="zope-cmf@lists.zope.org",
+      url="http://www.zope.org/Products/PluggableAuthService",
+      license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['Products'],
+      zip_safe=False,
+      #install_requires=['Zope >= 2.10']
+      entry_points="""
+      [zope2.initialize]
+      Products.PluggableAuthService = Products.PluggableAuthService:initialize
+      """,
+      )
