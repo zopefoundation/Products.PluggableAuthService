376c6c8e38bce7204cfbfcb37d8739e01b0a07fb
Jens Vagelpohl <jens@dataflake.org>
2008-09-05 08:16:59 +0000

    - cleanup

--- setup.py
+++ setup.py
@@ -2,29 +2,25 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
+NAME = 'PluggableAuthService'
+
 here = os.path.abspath(os.path.dirname(__file__))
-package = os.path.join(here, 'Products', 'PluggableAuthService')
+package = os.path.join(here, 'Products', NAME)
 
 def _package_doc(name):
     f = open(os.path.join(package, name))
     return f.read()
 
-NAME = 'PluggableAuthService'
-
-VERSION = _package_doc('version.txt').strip()
-if VERSION.startswith(NAME):
-    VERSION = VERSION[len(NAME):]
-while VERSION and VERSION[0] in '-_.':
-    VERSION = VERSION[1:]
-
-_boundary = '\n' + ('-' * 60) + '\n'
+_boundary = '\n' + ('-' * 60) + '\n\n'
 README = ( _package_doc('README.txt')
-         + _boundary + _package_doc('doc/CHANGES.txt')
-         + _boundary + "\nDownload\n========"
+         + _boundary 
+         + _package_doc('doc/CHANGES.txt')
+         + _boundary 
+         + "Download\n========"
          )
 
-setup(name='Products.PluggableAuthService',
-      version=VERSION,
+setup(name='Products.%s' % NAME,
+      version=_package_doc('version.txt').strip(),
       description='Pluggable Zope2 authentication / authorization framework',
       long_description=README,
       classifiers=[
@@ -41,7 +37,7 @@ setup(name='Products.PluggableAuthService',
         ],
       keywords='web application server zope zope2',
       author="Zope Corporation and contributors",
-      author_email="zope-cmf@lists.zope.org",
+      author_email="zope-cmf@zope.org",
       url="http://pypi.python.org/pypi/Products.PluggableAuthService",
       license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
       packages=find_packages(),
@@ -58,6 +54,6 @@ setup(name='Products.PluggableAuthService',
                      },
       entry_points="""
       [zope2.initialize]
-      Products.PluggableAuthService = Products.PluggableAuthService:initialize
-      """,
+      Products.%s = Products.%s:initialize
+      """ % (NAME, NAME),
       )
