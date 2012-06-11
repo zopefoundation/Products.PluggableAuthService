fcba9a8adaee9d1729451be037a1dc23add79565
Tres Seaver <tseaver@palladion.com>
2012-06-11 14:48:17 +0000

    Update to ZPL 2.1.

--- Products/PluggableAuthService/tests/test_exportimport.py
+++ Products/PluggableAuthService/tests/test_exportimport.py
@@ -4,7 +4,7 @@
 # Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
-# Version 2.0 (ZPL).  A copy of the ZPL should accompany this
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this
 # distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
