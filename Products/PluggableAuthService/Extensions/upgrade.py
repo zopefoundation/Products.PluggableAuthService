zopefoundation/Products.PluggableAuthService

commit eacb9ccbcee1331dfd64734c44f959386e99cbc1

Use relative imports

Products/PluggableAuthService/Extensions/upgrade.py
@@ -48,14 +48,14 @@ def _replaceUserFolder(self, RESPONSE=None):
     """
     from Acquisition import aq_base
 
-    from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin  # NOQA
-    from Products.PluggableAuthService.interfaces.plugins import IRoleAssignerPlugin  # NOQA
-    from Products.PluggableAuthService.interfaces.plugins import IRoleEnumerationPlugin  # NOQA
-    from Products.PluggableAuthService.interfaces.plugins import IRolesPlugin
-    from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin  # NOQA
-    from Products.PluggableAuthService.PluggableAuthService import PluggableAuthService  # NOQA
-    from Products.PluggableAuthService.plugins.ZODBRoleManager import ZODBRoleManager  # NOQA
-    from Products.PluggableAuthService.plugins.ZODBUserManager import ZODBUserManager  # NOQA
+    from .interfaces.plugins import IAuthenticationPlugin
+    from .interfaces.plugins import IRoleAssignerPlugin
+    from .interfaces.plugins import IRoleEnumerationPlugin
+    from .interfaces.plugins import IRolesPlugin
+    from .interfaces.plugins import IUserEnumerationPlugin
+    from .PluggableAuthService import PluggableAuthService
+    from .plugins.ZODBRoleManager import ZODBRoleManager
+    from .plugins.ZODBUserManager import ZODBUserManager
 
     if getattr(aq_base(self), '__allow_groups__', None):
         if self.__allow_groups__.__class__ is PluggableAuthService:

Products/PluggableAuthService/PropertiedUser.py
@@ -19,7 +19,7 @@ from AccessControl.User import BasicUser
 from Acquisition import aq_inner
 from Acquisition import aq_parent
 
-from Products.PluggableAuthService.interfaces.propertysheets import IPropertySheet  # NOQA
+from .interfaces.propertysheets import IPropertySheet
 
 from .interfaces.authservice import IPropertiedUser
 from .UserPropertySheet import UserPropertySheet

Products/PluggableAuthService/UserPropertySheet.py
@@ -19,7 +19,7 @@ import six
 from DateTime.DateTime import DateTime
 from OFS.Image import Image
 
-from Products.PluggableAuthService.interfaces.propertysheets import IPropertySheet  # NOQA
+from .interfaces.propertysheets import IPropertySheet
 from Products.PluggableAuthService.utils import classImplements
 
 

Products/PluggableAuthService/events.py
@@ -16,16 +16,16 @@ from zope.component import adapter
 from zope.component import subscribers
 from zope.interface import implementer
 
-from Products.PluggableAuthService.interfaces.authservice import IBasicUser
-from Products.PluggableAuthService.interfaces.events import ICredentialsUpdatedEvent  # NOQA
-from Products.PluggableAuthService.interfaces.events import IGroupCreatedEvent
-from Products.PluggableAuthService.interfaces.events import IGroupDeletedEvent
-from Products.PluggableAuthService.interfaces.events import IPASEvent
-from Products.PluggableAuthService.interfaces.events import IPrincipalAddedToGroupEvent  # NOQA
-from Products.PluggableAuthService.interfaces.events import IPrincipalCreatedEvent  # NOQA
-from Products.PluggableAuthService.interfaces.events import IPrincipalDeletedEvent  # NOQA
-from Products.PluggableAuthService.interfaces.events import IPrincipalRemovedFromGroupEvent  # NOQA
-from Products.PluggableAuthService.interfaces.events import IPropertiesUpdatedEvent  # NOQA
+from .interfaces.authservice import IBasicUser
+from .interfaces.events import ICredentialsUpdatedEvent
+from .interfaces.events import IGroupCreatedEvent
+from .interfaces.events import IGroupDeletedEvent
+from .interfaces.events import IPASEvent
+from .interfaces.events import IPrincipalAddedToGroupEvent
+from .interfaces.events import IPrincipalCreatedEvent
+from .interfaces.events import IPrincipalDeletedEvent
+from .interfaces.events import IPrincipalRemovedFromGroupEvent
+from .interfaces.events import IPropertiesUpdatedEvent
 
 
 @implementer(IPASEvent)

Products/PluggableAuthService/plugins/ChallengeProtocolChooser.py
@@ -20,15 +20,15 @@ from BTrees.OOBTree import OOBTree
 from zope.interface import Interface
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.PluggableAuthService.interfaces.plugins import IChallengePlugin
-from Products.PluggableAuthService.interfaces.plugins import IChallengeProtocolChooser  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import IRequestTypeSniffer  # NOQA
-from Products.PluggableAuthService.interfaces.request import IBrowserRequest
-from Products.PluggableAuthService.interfaces.request import IFTPRequest
-from Products.PluggableAuthService.interfaces.request import IWebDAVRequest
-from Products.PluggableAuthService.interfaces.request import IXMLRPCRequest
-from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.utils import classImplements
+from ..interfaces.plugins import IChallengePlugin
+from ..interfaces.plugins import IChallengeProtocolChooser
+from ..interfaces.plugins import IRequestTypeSniffer
+from ..interfaces.request import IBrowserRequest
+from ..interfaces.request import IFTPRequest
+from ..interfaces.request import IWebDAVRequest
+from ..interfaces.request import IXMLRPCRequest
+from ..plugins.BasePlugin import BasePlugin
+from ..utils import classImplements
 
 
 class IChallengeProtocolChooserPlugin(Interface):

Products/PluggableAuthService/plugins/CookieAuthHelper.py
@@ -37,12 +37,12 @@ from zope.interface import Interface
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
-from Products.PluggableAuthService.interfaces.plugins import IChallengePlugin
-from Products.PluggableAuthService.interfaces.plugins import ICredentialsResetPlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import ICredentialsUpdatePlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import ILoginPasswordHostExtractionPlugin  # NOQA
-from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.utils import classImplements
+from ..interfaces.plugins import IChallengePlugin
+from ..interfaces.plugins import ICredentialsResetPlugin
+from ..interfaces.plugins import ICredentialsUpdatePlugin
+from ..interfaces.plugins import ILoginPasswordHostExtractionPlugin
+from ..plugins.BasePlugin import BasePlugin
+from ..utils import classImplements
 
 
 class ICookieAuthHelper(Interface):

Products/PluggableAuthService/plugins/DelegatingMultiPlugin.py
@@ -26,13 +26,13 @@ from OFS.Folder import Folder
 from zope.interface import Interface
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import ICredentialsResetPlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import ICredentialsUpdatePlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
-from Products.PluggableAuthService.interfaces.plugins import IRolesPlugin
-from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.utils import classImplements
+from ..interfaces.plugins import IAuthenticationPlugin
+from ..interfaces.plugins import ICredentialsResetPlugin
+from ..interfaces.plugins import ICredentialsUpdatePlugin
+from ..interfaces.plugins import IPropertiesPlugin
+from ..interfaces.plugins import IRolesPlugin
+from ..plugins.BasePlugin import BasePlugin
+from ..utils import classImplements
 
 
 class IDelegatingMultiPlugin(Interface):

Products/PluggableAuthService/plugins/DomainAuthHelper.py
@@ -25,11 +25,11 @@ from BTrees.OOBTree import OOBTree
 from zope.interface import Interface
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin  # NOQA
-from Products.PluggableAuthService.interfaces.plugins import IExtractionPlugin
-from Products.PluggableAuthService.interfaces.plugins import IRolesPlugin
-from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.utils import classImplements
+from ..interfaces.plugins import IAuthenticationPlugin
+from ..interfaces.plugins import IExtractionPlugin
+from ..interfaces.plugins import IRolesPlugin
+from ..plugins.BasePlugin import BasePlugin
+from ..utils import classImplements
 
 
 try:

commit 2f5229b6ca1b073240ea1fd102586b3840c818e5

Fixed broken relative imports in Extensions/upgrade.py.
When adding a Plone Site:

```
Traceback (innermost last):
  Module ZPublisher.WSGIPublisher, line 162, in transaction_pubevents
  Module ZPublisher.WSGIPublisher, line 359, in publish_module
  Module ZPublisher.WSGIPublisher, line 262, in publish
  Module ZPublisher.mapply, line 85, in mapply
  Module ZPublisher.WSGIPublisher, line 63, in call_object
  Module Products.CMFPlone.browser.admin, line 273, in __call__
  Module Products.CMFPlone.factory, line 151, in addPloneSite
  Module Products.GenericSetup.tool, line 396, in runAllImportStepsFromProfile
   - __traceback_info__: profile-Products.CMFPlone:plone
  Module Products.GenericSetup.tool, line 1477, in _runImportStepsFromContext
  Module Products.GenericSetup.tool, line 1324, in _doRunHandler
  Module Products.CMFPlone.setuphandlers, line 144, in importFinalSteps
  Module Products.GenericSetup.tool, line 396, in runAllImportStepsFromProfile
   - __traceback_info__: profile-Products.CMFPlone:dependencies
  Module Products.GenericSetup.tool, line 1468, in _runImportStepsFromContext
  Module Products.GenericSetup.tool, line 1280, in _doRunImportStep
   - __traceback_info__: plonepas
  Module Products.PlonePAS.setuphandlers, line 494, in setupPlonePAS
  Module Products.PlonePAS.setuphandlers, line 482, in installPAS
  Module Products.PlonePAS.setuphandlers, line 301, in migrate_root_uf
  Module Products.PluggableAuthService.Extensions.upgrade, line 187, in replace_acl_users
  Module Products.PluggableAuthService.Extensions.upgrade, line 51, in _replaceUserFolder
ImportError: No module named interfaces.plugins
```

This was broken since eacb9ccbcee1331dfd64734c44f959386e99cbc1 which switched to relative imports.
Looks like this was the only file where it went wrong.

Products/PluggableAuthService/Extensions/upgrade.py
@@ -48,14 +48,14 @@ def _replaceUserFolder(self, RESPONSE=None):
     """
     from Acquisition import aq_base
 
-    from .interfaces.plugins import IAuthenticationPlugin
-    from .interfaces.plugins import IRoleAssignerPlugin
-    from .interfaces.plugins import IRoleEnumerationPlugin
-    from .interfaces.plugins import IRolesPlugin
-    from .interfaces.plugins import IUserEnumerationPlugin
-    from .PluggableAuthService import PluggableAuthService
-    from .plugins.ZODBRoleManager import ZODBRoleManager
-    from .plugins.ZODBUserManager import ZODBUserManager
+    from ..interfaces.plugins import IAuthenticationPlugin
+    from ..interfaces.plugins import IRoleAssignerPlugin
+    from ..interfaces.plugins import IRoleEnumerationPlugin
+    from ..interfaces.plugins import IRolesPlugin
+    from ..interfaces.plugins import IUserEnumerationPlugin
+    from ..PluggableAuthService import PluggableAuthService
+    from ..plugins.ZODBRoleManager import ZODBRoleManager
+    from ..plugins.ZODBUserManager import ZODBUserManager
 
     if getattr(aq_base(self), '__allow_groups__', None):
         if self.__allow_groups__.__class__ is PluggableAuthService:
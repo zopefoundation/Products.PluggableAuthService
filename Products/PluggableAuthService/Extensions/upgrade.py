2f5229b6ca1b073240ea1fd102586b3840c818e5
Maurits van Rees <maurits@vanrees.org>
2020-09-01 12:09:26 +0200

    Fixed broken relative imports in Extensions/upgrade.py.
    When adding a Plone Site:
    
    ```
    Traceback (innermost last):
      Module ZPublisher.WSGIPublisher, line 162, in transaction_pubevents
      Module ZPublisher.WSGIPublisher, line 359, in publish_module
      Module ZPublisher.WSGIPublisher, line 262, in publish
      Module ZPublisher.mapply, line 85, in mapply
      Module ZPublisher.WSGIPublisher, line 63, in call_object
      Module Products.CMFPlone.browser.admin, line 273, in __call__
      Module Products.CMFPlone.factory, line 151, in addPloneSite
      Module Products.GenericSetup.tool, line 396, in runAllImportStepsFromProfile
       - __traceback_info__: profile-Products.CMFPlone:plone
      Module Products.GenericSetup.tool, line 1477, in _runImportStepsFromContext
      Module Products.GenericSetup.tool, line 1324, in _doRunHandler
      Module Products.CMFPlone.setuphandlers, line 144, in importFinalSteps
      Module Products.GenericSetup.tool, line 396, in runAllImportStepsFromProfile
       - __traceback_info__: profile-Products.CMFPlone:dependencies
      Module Products.GenericSetup.tool, line 1468, in _runImportStepsFromContext
      Module Products.GenericSetup.tool, line 1280, in _doRunImportStep
       - __traceback_info__: plonepas
      Module Products.PlonePAS.setuphandlers, line 494, in setupPlonePAS
      Module Products.PlonePAS.setuphandlers, line 482, in installPAS
      Module Products.PlonePAS.setuphandlers, line 301, in migrate_root_uf
      Module Products.PluggableAuthService.Extensions.upgrade, line 187, in replace_acl_users
      Module Products.PluggableAuthService.Extensions.upgrade, line 51, in _replaceUserFolder
    ImportError: No module named interfaces.plugins
    ```
    
    This was broken since eacb9ccbcee1331dfd64734c44f959386e99cbc1 which switched to relative imports.
    Looks like this was the only file where it went wrong.

--- Products/PluggableAuthService/Extensions/upgrade.py
+++ Products/PluggableAuthService/Extensions/upgrade.py
@@ -48,14 +48,14 @@ def _replaceUserFolder(self, RESPONSE=None):
     """
     from Acquisition import aq_base
 
-    from .interfaces.plugins import IAuthenticationPlugin
-    from .interfaces.plugins import IRoleAssignerPlugin
-    from .interfaces.plugins import IRoleEnumerationPlugin
-    from .interfaces.plugins import IRolesPlugin
-    from .interfaces.plugins import IUserEnumerationPlugin
-    from .PluggableAuthService import PluggableAuthService
-    from .plugins.ZODBRoleManager import ZODBRoleManager
-    from .plugins.ZODBUserManager import ZODBUserManager
+    from ..interfaces.plugins import IAuthenticationPlugin
+    from ..interfaces.plugins import IRoleAssignerPlugin
+    from ..interfaces.plugins import IRoleEnumerationPlugin
+    from ..interfaces.plugins import IRolesPlugin
+    from ..interfaces.plugins import IUserEnumerationPlugin
+    from ..PluggableAuthService import PluggableAuthService
+    from ..plugins.ZODBRoleManager import ZODBRoleManager
+    from ..plugins.ZODBUserManager import ZODBUserManager
 
     if getattr(aq_base(self), '__allow_groups__', None):
         if self.__allow_groups__.__class__ is PluggableAuthService:
